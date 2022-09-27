b682d9b14f0535954275444951201e80459320c4
louisljamie <louis.l.jamie@gmail.com>
2022-09-27 11:23:50 -0500

    Started

---
+++ advanced_dataframes.py
@@ -0,0 +1,23 @@
+# Run python -m pip install pymysql from your terminal to install the mysql client (any folder is fine)
+# cd into your exercises folder for this module and run echo env.py >> .gitignore
+
+# Create a function named get_db_url. It should accept a username, hostname, password, and database name and return a url connection string formatted like in the example at the start of this lesson.
+
+# Use your function to obtain a connection to the employees database.
+
+#Once you have successfully run a query:
+# a. Intentionally make a typo in the database url. What kind of error message do you see?
+
+#b. Intentionally make an error in your SQL query. What does the error message look like?
+
+# Read the employees and titles tables into two separate DataFrames.
+
+# How many rows and columns do you have in each DataFrame? Is that what you expected?
+
+# Display the summary statistics for each DataFrame.
+
+# How many unique titles are in the titles DataFrame?
+
+# What is the oldest date in the to_date column?
+
+# What is the most recent date in the to_date column?
